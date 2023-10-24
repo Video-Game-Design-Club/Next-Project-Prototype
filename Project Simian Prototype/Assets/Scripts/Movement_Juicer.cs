0678c34284d489268030243889d353425c227cbd
nate21jones12 <nate21jones12@comcast.net>
2023-10-24 11:54:19 -0700

    movement and camera 90% complete

--- Project Simian Prototype/Assets/Scripts/Movement_Juicer.cs
+++ Project Simian Prototype/Assets/Scripts/Movement_Juicer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -24,6 +25,11 @@ public class Movement_Juicer : MonoBehaviour
     public float airAcceleration;
     public float speed;
     public float jumpForce;
+
+    [Header("Movement Spice")]
+    public float turnSmoothTime = 0.1f;
+    float dampHolder;
+    public Transform cam;
 
     public enum State
     {
@@ -67,12 +73,29 @@ public class Movement_Juicer : MonoBehaviour
     void doMove()
     {
         //functionality
-        targetVec.Set(rawInput.x * speed, 0f, rawInput.y * speed);
-        currentVec.Set(rb.velocity.x, 0f, rb.velocity.z);
         Vector3 juicedVec = new Vector3();
-        juicedVec = targetVec - currentVec;
 
-        rb.AddForce(juicedVec * acceleration);
+        targetVec.Set(rawInput.x, 0f, rawInput.y);
+
+        /*targetVec.Set(rawInput.x * speed, 0f, rawInput.y * speed);
+        currentVec.Set(rb.velocity.x, 0f, rb.velocity.z);*/
+
+        float targetAngle = Mathf.Atan2(rawInput.x, rawInput.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref dampHolder, turnSmoothTime);
+
+        //targetVec = Quaternion.Euler(0f, targetAngle, 0f) * targetVec;
+        
+        /*juicedVec = targetVec - currentVec;*/
+        
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+        Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+        
+        if (targetVec != Vector3.zero)
+        {
+            rb.AddForce(moveDir.normalized * acceleration);
+        }
+            
 
         //switch states
         if(rb.velocity.magnitude <= 0.1f) 
@@ -103,12 +126,20 @@ public class Movement_Juicer : MonoBehaviour
     void doFall()
     {
         //functionality
-        targetVec.Set(rawInput.x * speed, 0f, rawInput.y * speed);
-        currentVec.Set(rb.velocity.x, 0f, rb.velocity.z);
-        Vector3 juicedVec = new Vector3();
-        juicedVec = targetVec - currentVec;
+            //Rotates Player based on camera angle and input
+        float targetAngle = Mathf.Atan2(rawInput.x, rawInput.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref dampHolder, turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-        rb.AddForce(juicedVec * airAcceleration);
+            //moves character along a vector that is rotated to be pointing the direction of the target angle trajectory
+        targetVec.Set(rawInput.x, 0f, rawInput.y);
+        Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            //checks if user has stopped inputting values because transfering back to idle state is dependent on speed not input
+        if (targetVec != Vector3.zero)
+        {
+            rb.AddForce(moveDir.normalized * airAcceleration);
+        }
 
         //switch states
         if (onGround && rb.velocity.magnitude < 0.1f)
