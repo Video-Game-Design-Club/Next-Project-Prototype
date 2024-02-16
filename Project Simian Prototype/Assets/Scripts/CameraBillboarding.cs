f09a34175186e4c9cc522502a32bd1a3b3fe2d34
M Saechao <55562924+Contemplationjpg@users.noreply.github.com>
2024-02-15 22:43:59 -0800

    Update CameraBillboarding.cs

--- Project Simian Prototype/Assets/Scripts/CameraBillboarding.cs
+++ Project Simian Prototype/Assets/Scripts/CameraBillboarding.cs
@@ -6,30 +6,62 @@ using UnityEngine;
 
 public class CameraBillboarding : MonoBehaviour
 {
+    public Cinemachine.CinemachineFreeLook thirdpersoncam;
 
-     public Transform cam;
-    // public CinemachineFreeLook cam;
+    public Camera cam;
+    public Transform floor;
+
+    private Quaternion camFace; 
+
+    private Quaternion standing; 
 
     void Awake()
     {
-       
+        thirdpersoncam = GetComponent<CinemachineFreeLook>();
     }
 
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        standing = new Quaternion();
     }
 
-    // Update is called once per frame
     void Update()
     {
 
-        // transform.(cam.transform.eulerAngles);
+        
+        //transform.right = Camera.main.transform.right;
+        //transform.up = Camera.main.transform.up + standing;
+        //transform.right = standing;
 
-        transform.LookAt(cam.transform.position, Vector3.up);
-        // transform.Rotate(Vector3.down);
-        // transform.LookAt(cam.transform.position, Vector3.right);
-        // transform.LookAt(cam.transform.position, Vector3.forward); 
+
+
+        
+        //transform.rotation = Quaternion.LookRotation(-cam.transform.up);
+        
+        //looks at camera then rotates 90 degrees on x axis (this works)
+
+        Vector3 a = cam.transform.position;
+        a.y = transform.position.y;
+        transform.LookAt(a); //,Vector3.up
+        transform.Rotate(90,0,0);
+        //transform.rotation;
+        
+        
+
+
+        //transform.RotateAround();
+/*
+    rotates towards rotation of camera (camera doesn't rotate)
+        transform.rotation = camFace; 
+        camFace = cam.transform.rotation;
+        */
+
+        //transform.Rotate(cam.eulerAngles);
+
+        
+
+        // transform.rotation = Quaternion.RotateTowards(transform.up, cam.transform.up);
+        // transform.LookAt(floor.transform.position);
     }
 }
