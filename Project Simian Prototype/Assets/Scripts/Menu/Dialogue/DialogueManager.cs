cb30e9bc208e26d34c245b05eb168d6d8cd44b13
Malakai Saechao <55562924+Contemplationjpg@users.noreply.github.com>
2024-03-28 15:09:13 -0700

    Revert "messed with dialogue more"
    This reverts commit 122ab906fad6c07cc5ff63023272053fefa1544f.

--- Project Simian Prototype/Assets/Scripts/Menu/Dialogue/DialogueManager.cs
+++ Project Simian Prototype/Assets/Scripts/Menu/Dialogue/DialogueManager.cs
@@ -1,37 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
-
-    public TextMeshProUGUI charName; //textbox for character name
-    public TextMeshProUGUI charText; //textbox for character dialogue
-   
-    public string charFileName;
-    public string charFileNum;
-
-   void PrintDialogue()
-   {
-         foreach (string line in ReadDialogue(charFileName,charFileNum))
-         {
-            charText.text = line;
-         }
-   }
-
-   List<string> ReadDialogue(string name, string number)
+    // Start is called before the first frame update
+    void Start()
     {
-        string filePath = Application.streamingAssetsPath + "/Dialogue/" + name + number + ".txt";
-        List<string> output = File.ReadAllLines(filePath).ToList();
-        return output; 
+        
     }
 
-
-
-
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
