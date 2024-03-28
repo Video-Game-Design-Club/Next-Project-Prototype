122ab906fad6c07cc5ff63023272053fefa1544f
Malakai Saechao <55562924+Contemplationjpg@users.noreply.github.com>
2024-03-28 14:54:22 -0700

    messed with dialogue more

---
+++ Project Simian Prototype/Assets/Scripts/Menu/Dialogue/Dialogue.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue
+{
+    public string name;
+    public string[] filenumbers;
+
+}
