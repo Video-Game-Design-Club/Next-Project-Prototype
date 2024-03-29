fc64e0a18a7e560b17512b799f81bd3796082fee
Malakai Saechao <55562924+Contemplationjpg@users.noreply.github.com>
2024-03-28 23:08:04 -0700

    Added Evil Billy Button

--- Project Simian Prototype/Assets/Scripts/Menu/Dialogue/DialogueManager.cs
+++ Project Simian Prototype/Assets/Scripts/Menu/Dialogue/DialogueManager.cs
@@ -10,6 +10,7 @@ using UnityEngine.WSA;
 
 public class DialogueManager : MonoBehaviour
 {
+    public float textSpeed = 01f;
     public GameObject dialogBox;
     public string charName;
     public TMP_Text textBox;
@@ -19,7 +20,8 @@ public class DialogueManager : MonoBehaviour
     private string[] lines;
     private Queue<string> queuedLines;
     private int index = 0;
-    
+    private string tempLine;
+    private string typedName;    
     
 
     public void PrintDialogue()
@@ -35,24 +37,26 @@ public class DialogueManager : MonoBehaviour
                 ReadDialogue();
                 return;
             }
-
-            string tempLine = queuedLines.Dequeue();
+            tempLine = queuedLines.Dequeue();
             Debug.Log(tempLine);
-
             if (tempLine.Contains("[NAME="))
             {
                 string tempName = tempLine.Remove(0,6);
                 tempName = tempName.Remove(tempName.IndexOf("]"));
                 Debug.Log(tempName);
+                typedName = tempName;
                 nameTextBox.SetText(tempName);
                 index +=1;
                 PrintDialogue();
             }
-            else
+        else
             {
-                textBox.SetText(tempLine);
+                nameTextBox.SetText(typedName);
                 index+=1;
+                textBox.SetText(tempLine);
             }
+            
+            
     }
 
     void ReadDialogue()
@@ -82,7 +86,61 @@ public class DialogueManager : MonoBehaviour
         index=0;
         queuedLines.Clear();
     }
+
+    // void NextLine()
+    // {
+
+    //     if (tempLine.Contains("[NAME="))
+    //         {
+    //             string tempName = tempLine.Remove(0,6);
+    //             tempName = tempName.Remove(tempName.IndexOf("]"));
+    //             Debug.Log(tempName);
+    //             nameTextBox.SetText(tempName);
+    //             index +=1;
+    //             PrintDialogue();
+    //         }
+    //     else
+    //         {
+    //             index+=1;
+    //             textBox.SetText("");
+    //             while (!Input.anyKey)
+    //             {
+    //                 foreach (char c in tempLine.ToCharArray())
+    //                 {
+    //                     textBox.SetText(textBox.text+c);
+    //                     Wait(textSpeed);
+    //                 }
+    //             }
+                
+    //         }
+        
+
+    // }
+
+    // IEnumerable Wait(float time)
+    // {
+    //     yield return new WaitForSeconds(time);
+    // }
     
+    // IEnumerable TypeText(string message)
+    // {
+    //     char[] tempChars = message.ToCharArray();
+    //     string currentText = "";
+    //     foreach (char c in message.ToCharArray())
+    //     {
+    //         textBox.SetText(currentText+tempChars[c].ToString());
+    //         currentText = textBox.GetComponent<TMP_InputField>().text;
+    //         if (Input.GetKeyDown(KeyCode.KeypadEnter))
+    //         {
+    //             textBox.SetText(message);
+    //             yield return 0;
+    //         }
+    //         yield return new WaitForSeconds(textSpeed);
+    //     }
+    //     yield return 0;
+    // }
+
+
     void Start()
     {
         index = 0;
