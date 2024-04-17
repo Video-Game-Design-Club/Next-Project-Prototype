2669c0b469b9c934f8ebc371ac3e7d328385346b
JustBacon <christianbuco17@gmail.com>
2024-04-17 16:42:04 -0700

    horizontal speed implemented
    the speed from movementJuicer is set to 0 and uses glideSpeed instead.

--- Project Simian Prototype/Assets/Scripts/Glide.cs
+++ Project Simian Prototype/Assets/Scripts/Glide.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 // Manipulates the speed and gravity of the player to glide.
+// glidespeed/thrust, lift, gravity, no drag
+
+// glide is set to toggle. left mouse button.
+// glide has a duration (cant glide forever)
+
 public class Glide : MonoBehaviour
 {
     Movement_Juicer juice;
     Rigidbody rb;
-    [SerializeField] public float glideSpeed;       // speed change when gliding
+    [SerializeField] public float glideSpeed;       // speed change when gliding (thrust)
     [SerializeField] public float lift;             // works against gravity
     private float initialSpeed;                     // stores original speed from Movement_Juicer.cs
     private bool isGliding;                         // check if player is gliding
-    private bool zeroVelLock;                       // used in FixedUpdate, makes the velocity zero once
+    private bool zeroVelLock;                       // used in FixedUpdate, makes the velocity zero once. velocity in the y-direction
     private int glideCounter;                       // used so player cant spam glide ability to reset duration
 
     // Duration for glide
@@ -54,8 +59,9 @@ public class Glide : MonoBehaviour
                 rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
                 zeroVelLock = true;
             }
-            rb.AddForce(Vector3.up * lift);
-            juice.speed = glideSpeed;
+            juice.speed = 0;
+            rb.AddForce(Vector3.up * lift);         // lift
+            rb.AddRelativeForce(0f,0f,glideSpeed);  // thrust (horizontal speed constant)
         }
         
         // if player is on the ground then speed back to normal
@@ -64,6 +70,7 @@ public class Glide : MonoBehaviour
         }
     }
 
+    // glide will reset if player touches ground
     private void resetGlide(){
             remainingTime = glideAbilityDuration;
             juice.speed = initialSpeed;
